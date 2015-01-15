560b9a0649221cf57d1a44b2be417f644339f68b
mlwmlw <520cutecat@gmail.com>
2015-01-16 02:12:40 +0800

    pickerdate css

--- SuperCalendar.jsx
+++ SuperCalendar.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
 var _ = require('lodash');
+var DATES = {
+	compare: function(a, b) {
+		return a.getFullYear() == b.getFullYear() &&
+			a.getMonth() == b.getMonth() &&
+			a.getDate() == b.getDate();
+	}
+}
 var Day = React.createClass({
 	render: function() {
-		style = this.props.selected ? {background: 'red'}: {};
-		return <td style={style}>{this.props.day}</td>;
+		var date = this.props.date;
+		var today = new Date();
+		var cls = "picker__day picker__day--infocus";
+		if(this.props.selected)
+			cls += " picker__day--selected picker__day--highlighted";
+		if(DATES.compare(date, today))
+			cls += " picker__day--today";
+		return <td onClick={this.props.onClick.bind(this, date)}><div className={cls}>{date.getDate()}</div></td>;
 	}
 });
 var Week = React.createClass({
@@ -11,21 +24,44 @@ var Week = React.createClass({
 		var weekDate = new Date(this.props.year, this.props.month - 1, this.props.date - 1);
 		var days = [];
 		var selected = this.props.selected;
-		for(var i = 0; i < 7; i++) {
-			weekDate.setDate(weekDate.getDate() + 1);
-			var day = weekDate.getMonth() + 1 == this.props.month && weekDate.getDate();
-			if(selected.getFullYear() == weekDate.getFullYear() &&
-				selected.getMonth() == weekDate.getMonth() &&
-				selected.getDate() == weekDate.getDate())
-				days.push(<Day selected month={this.props.month} day={day}></Day>);
+		for(var i = 1; i <= 7; i++) {
+			var theDate = new Date(weekDate.getTime() + 86400 * i * 1000)
+			//var day = weekDate.getMonth() + 1 == this.props.month && weekDate.getDate();
+			if(DATES.compare(theDate, selected))
+				days.push(<Day onClick={this.props.onClick} selected date={theDate}></Day>);
 			else
-				days.push(<Day month={this.props.month} day={day}></Day>);
+				days.push(<Day onClick={this.props.onClick} date={theDate}></Day>);
 		}
 		return <tr>{days}</tr>;
 	}
 });
-
-
+var WeekHead = React.createClass({
+	render: function() {
+		return <thead>
+					<tr>
+						<th className="picker__weekday">日</th>
+						<th className="picker__weekday">一</th>
+						<th className="picker__weekday">二</th>
+						<th className="picker__weekday">三</th>
+						<th className="picker__weekday">四</th>
+						<th className="picker__weekday">五</th>
+						<th className="picker__weekday">六</th>
+					</tr>
+					</thead>;
+	}
+});
+var WeekFoot = React.createClass({
+	render: function() {
+		return <tfoot style={{textAlign:"center"}}>
+						<tr>
+							<td colSpan="7">
+								<button onClick={this.props.prev}>prev</button>
+								<button onClick={this.props.next}>next</button>
+							</td>
+						</tr>
+					</tfoot>;
+	}
+});
 var Calendar = React.createClass({
 	getMonthDay: function(m) {
 		m = m || this.state.view.getMonth();
@@ -52,41 +88,53 @@ var Calendar = React.createClass({
 		date.setDate(1);
 		return date;
 	},
+	getMonthName: function(i) {
+		var map = ["January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"];
+		return map[i];
+	},
+	select: function(date) {
+		this.setState({selected: date});
+	},
 	render: function() {
 		var date = this.firstDay();
 		day = this.getMonthDay() + date.getDay();
 		weeks = [];
 		for(var i =0; i < Math.ceil(day / 7); i++) {
-			weeks[i] = <Week selected={this.state.selected} week={i} date={date.getDate() + i * 7 - date.getDay()} year={date.getFullYear()} month={date.getMonth() + 1}></Week>;
+			weeks[i] = <Week onClick={this.select} selected={this.state.selected} week={i} date={date.getDate() + i * 7 - date.getDay()} year={date.getFullYear()} month={date.getMonth() + 1}></Week>;
 		}
 		return (
-			<div>
-				<table>
-					<caption>{this.state.view.getFullYear() + ' 年 ' + (this.state.view.getMonth() + 1) + ' 月'}</caption>
-					<thead>
-					<tr>
-						<th>日</th>
-						<th>一</th>
-						<th>二</th>
-						<th>三</th>
-						<th>四</th>
-						<th>五</th>
-						<th>六</th>
-					</tr>
-					</thead>
-					<tfoot style={{textAlign:"center"}}>
-						<tr>
-							<td colSpan="7">
-								<button onClick={this.prev}>prev</button>
-								<button onClick={this.next}>next</button>
-							</td>
-						</tr>
-					</tfoot>
+			<div className="picker--focused picker--opened">
+			<div className="picker__holder">
+				<div className="picker__frame">
+				<div className="picker__wrap">
+				<div className="picker__box">
+				<div className="picker__header">
+					<div className="picker__month">{this.getMonthName(this.state.view.getMonth())}</div>
+					<div className="picker__year">{this.state.view.getFullYear()}</div>
+					<div className="picker__nav--prev" onClick={this.prev} role="button" title="Previous month"> </div>
+					<div className="picker__nav--next" onClick={this.next} role="button" title="Next month"> </div>
+				</div>
+				<table className="picker__table">
+					<WeekHead />
 					<tbody>
 					{weeks}
 					</tbody>
-					
 				</table>
+				</div>
+				</div>
+				</div>
+			</div>
 			</div>
 		);
 	}
