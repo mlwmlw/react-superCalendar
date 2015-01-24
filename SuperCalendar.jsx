990bd69b4546355b6c060d1bf34579a4990b36d4
mlwmlw <520cutecat@gmail.com>
2015-01-25 00:19:58 +0800

    merge

--- SuperCalendar.jsx
+++ SuperCalendar.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var _ = require('lodash');
 var DATES = {
 	compare: function(a, b) {
+		if(!a || !b)
+			return false;
 		return a.getFullYear() == b.getFullYear() &&
 			a.getMonth() == b.getMonth() &&
 			a.getDate() == b.getDate();
@@ -21,9 +23,9 @@ var Day = React.createClass({
 
 		if(this.props.selected)
 			cls += " picker__day--selected picker__day--highlighted";
-		if(DATES.compare(date, today))
-			cls += " picker__day--today";
-		return <td onClick={this.props.onClick.bind(this, date)}><div className={cls}>{date.getDate()}</div></td>;
+		else if(DATES.compare(date, today))
+			cls += " picker__day--today picker__day--highlighted";
+		return <td onClick={this.props.onClick.bind(null, date)}><div className={cls}>{date.getDate()}</div></td>;
 	}
 });
 var Week = React.createClass({
@@ -34,10 +36,7 @@ var Week = React.createClass({
 		for(var i = 0; i < 7; i++) {
 			var theDate = new Date(weekDate.getTime() + 86400 * i * 1000)
 			//var day = weekDate.getMonth() + 1 == this.props.month && weekDate.getDate();
-			if(DATES.compare(theDate, selected))
-				days.push(<Day onClick={this.props.onClick} week={this.props.week} month={weekDate.getMonth()} selected date={theDate}></Day>);
-			else
-				days.push(<Day onClick={this.props.onClick} week={this.props.week} month={weekDate.getMonth()} date={theDate}></Day>);
+			days.push(<Day selected={DATES.compare(theDate, selected)} onClick={this.props.onClick} week={this.props.week} month={weekDate.getMonth()} date={theDate}></Day>);
 		}
 		return <tr>{days}</tr>;
 	}
@@ -69,6 +68,47 @@ var WeekFoot = React.createClass({
 					</tfoot>;
 	}
 });
+var Time = React.createClass({
+	render: function() {
+		var i = this.props.children;
+		return <div itemProp={this.props.deg} key={i} style={{position: "absolute", top: "45%", left: "3%", width: "47%", transformOrigin: "right center", height: "1em", transform: "rotate(" + this.props.deg + "deg)"}}>
+					<span style={{position: "absolute", transform: "rotate(" + -this.props.deg + "deg)"}}>{i> 0? i: 12}</span>
+			</div>
+	}
+});
+var Times = React.createClass({
+	render: function() {
+		var time = [];
+		for(var i = 0; i< 12; i++) {
+			var deg = i * 30 + 90;
+			time.push(<Time deg={deg}>{i}</Time>);
+		}
+		return <div>{time}</div>;
+	}
+});
+var Clock = React.createClass({
+	getInitialState: function () {
+		return {deg: 0};
+	},
+	start: function(e) {
+		this.setState({drag: true});
+		this.setState({deg: +e.target.getAttribute('itemprop')});
+	},
+	end: function() {
+		this.setState({drag: false});
+	},
+	move: function(e) {
+		if(this.state.drag)
+			this.setState({deg: +e.target.getAttribute('itemprop')});
+	},
+	render: function() {
+		return <div onMouseUp={this.end} onMouseMove={this.move} onMouseDown={this.start} style={{width: "150px", height: "150px", borderRadius: "150px", background: "#eee", border: "1px solid gray", marginTop: "300px", position: "relative", cursor: "pointer", WebkitUserSelect: "none"}}>
+			<div className="point" style={{background: "#0095dd", borderRadius: 3, width: 3, height: 3, position: "absolute", top: "50%", left: "50%"}}></div>
+			<div className="line" style={{border: "1px #c0e5f7 solid", marginTop: "50%", width: "45%", marginLeft: "5%", transform: "rotate(" + this.state.deg + "deg)", transformOrigin: "right center"}}></div>
+			<Times />
+		</div>
+	}
+});
 var Calendar = React.createClass({
 	getMonthDay: function(m) {
 		m = m || this.state.view.getMonth();
@@ -78,7 +118,9 @@ var Calendar = React.createClass({
 		return date.getDate() + 1;
 	},
 	getInitialState: function() {
-		return {view: new Date(), selected: new Date()};
+		var state = {view: new Date(), selected: new Date()};
+		state.selected = null;
+		return state;
 	},
 	prev: function() {
 		var date = this.state.view;
@@ -123,27 +165,30 @@ var Calendar = React.createClass({
 			weeks[i] = <Week onClick={this.select} selected={this.state.selected} week={i} weekDate={weekDate}></Week>;
 		}
 		return (
-			<div className="picker--focused picker--opened">
-			<div className="picker__holder">
-				<div className="picker__frame">
-				<div className="picker__wrap">
-				<div className="picker__box">
-				<div className="picker__header">
-					<div className="picker__month">{this.getMonthName(this.state.view.getMonth())}</div>
-					<div className="picker__year">{this.state.view.getFullYear()}</div>
-					<div className="picker__nav--prev" onClick={this.prev} role="button" title="Previous month"> </div>
-					<div className="picker__nav--next" onClick={this.next} role="button" title="Next month"> </div>
+			<div>
+				<div className="picker--focused picker--opened" style={{position: "relative", height: 10}}>
+					<div className="picker__holder">
+						<div className="picker__frame">
+							<div className="picker__wrap">
+								<div className="picker__box">
+									<div className="picker__header">
+										<div className="picker__month">{this.getMonthName(this.state.view.getMonth())}</div>
+										<div className="picker__year">{this.state.view.getFullYear()}</div>
+										<div className="picker__nav--prev" onClick={this.prev} role="button" title="Previous month"> </div>
+										<div className="picker__nav--next" onClick={this.next} role="button" title="Next month"> </div>
+									</div>
+									<table className="picker__table">
+										<WeekHead />
+										<tbody>
+										{weeks}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<table className="picker__table">
-					<WeekHead />
-					<tbody>
-					{weeks}
-					</tbody>
-				</table>
-				</div>
-				</div>
-				</div>
-			</div>
+				<Clock />
 			</div>
 		);
 	}
